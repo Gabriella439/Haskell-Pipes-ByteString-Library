159bcec2d68c317203a55386b7f23f0bf6db8eb5
Gabriel Gonzalez <Gabriel439@gmail.com>
2017-07-03 08:19:53 -0700

    Add `breakOn`.  Fixes #27

--- default.nix
+++ default.nix
@@ -1,12 +1,13 @@
 { mkDerivation, base, bytestring, pipes, pipes-group, pipes-parse
-, stdenv, transformers
+, stdenv, stringsearch, transformers
 }:
 mkDerivation {
   pname = "pipes-bytestring";
   version = "2.1.5";
   src = ./.;
   libraryHaskellDepends = [
-    base bytestring pipes pipes-group pipes-parse transformers
+    base bytestring pipes pipes-group pipes-parse stringsearch
+    transformers
   ];
   description = "ByteString support for pipes";
   license = stdenv.lib.licenses.bsd3;
