895ffa4b6421b1650f8a9071402dfd72308ee4a2
Gabriel Gonzalez <Gabriel439@gmail.com>
2017-07-13 21:42:31 -0700

    Version 2.1.5 => 2.1.6

--- default.nix
+++ default.nix
@@ -3,7 +3,7 @@
 }:
 mkDerivation {
   pname = "pipes-bytestring";
-  version = "2.1.5";
+  version = "2.1.6";
   src = ./.;
   libraryHaskellDepends = [
     base bytestring pipes pipes-group pipes-parse stringsearch
